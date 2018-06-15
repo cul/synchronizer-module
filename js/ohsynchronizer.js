616a90cc062468301a6cba392b158749339dea75
Benjamin Armintor <armintor@gmail.com>
2018-06-15 13:11:06 -0400

    WIP move into js modules for use in other apps

--- js/ohsynchronizer.js
+++ js/ohsynchronizer.js
@@ -1,19 +1,24 @@
 /* Columbia University Library
-   Project: Synchronizer Module
-   File: script.js
-	 Description: Javascript functions providing file upload and display
-   Author: Ashley Pressley
-   Date: 05/23/2018
-	 Version: 1.0
+	Project: Synchronizer Module
+	File: script.js
+	Description: Javascript functions providing file upload and display
+	Authors: Ashley Pressley, Benjamin Armintor
+	Date: 05/23/2018
+	Version: 1.0
 */
 
 /** Global variables **/
 // Here we embed the empty YouTube video player
 // This must be presented before any function that can utilize it
-window.ytplayer = null;
-function onYouTubeIframeAPIReady() {}
 
 var OHSynchronizer = function(){};
+
+// get the relative path of this file, to find WebWorker modules later
+OHSynchronizer.webWorkers = $("script[src$='ohsynchronizer.js']").attr('src').replace(/\.js.*$/,'');
+
+OHSynchronizer.onYouTubeIframeAPIReady = function() {}
+
+OHSynchronizer.ytplayer = null;
 
 // Looping is used to notify various functions if Transcript looping is currently active
 OHSynchronizer.looping = -1;
@@ -443,7 +448,7 @@ OHSynchronizer.Import.renderText = function(file, ext) {
 
 							// We implement a Web Worker because larger transcript files will freeze the browser
 							if (window.Worker) {
-								var textWorker = new Worker("./ohsynchronizer/transcript.js");
+								var textWorker = new Worker(OHSynchronizer.webWorkers + "/transcript.js");
 								textWorker.postMessage(text);
 								textWorker.onmessage = function(e) {
 									document.getElementById('transcript').innerHTML += e.data;
@@ -1301,84 +1306,3 @@ function closeButtons() {
 	}
 }
 
-// Document Ready
-(function($){
-	// Don't show the A/V controls, errorBar, or Tag button
-	$("#video").hide();
-	$("#audio").hide();
-	$("#tag-segment-btn").hide();
-	$("#tag-controls-ap").hide();
-	$("#tag-controls-yt").hide();
-	$("#sync-controls").hide();
-	$("#finish-area").hide();
-	$("#transcript-preview").hide();
-
-	// Initialize close buttons, tabs, and accordion
-	closeButtons();
-
-  $("#text-tabs").tabs({
-		active: 0
-	});
-
-	// If the Index tab is clicked, ensure transcript looping is deactivated
-	$('a[href$="tabs-index"]').click(function () {
-		looping = -1;
-	});
-
-	// Activate our index and preview index accordions
-	$("#indexAccordion").accordion({
-    header: "> div > h3",
-    autoHeight: false,
-    collapsible: true,
-		clearStyle: true,
-    active: false
-  });
-
-	// Watch the AblePlayer time status for Transcript Syncing
-	document.getElementById("video-player").ontimeupdate = function() { OHSynchronizer.AblePlayer.transcriptTimestamp() };
-	document.getElementById("audio-player").ontimeupdate = function() { OHSynchronizer.AblePlayer.transcriptTimestamp() };
-
-	// Disallow non-numerical values in transcript controls
-	// Only allow 0-9, backspace, and delete
-	$('#sync-roll').keypress(function (event) {
-    if (event.shiftKey == true) { event.preventDefault(); }
-    if ((event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 8 || event.keyCode == 46 || event.keyCode == 37 || event.keyCode == 39) { }
-		else { event.preventDefault(); }
-	});
-
-	// Never let the transcript roll control be empty
-	$('#sync-roll').blur(function () {
-		if(!$(this).val()) { $(this).val('0'); }
-	});
-
-	// Update the Tag Segment timestamp when the modal opens from Add Segment
-	$('#tag-segment-btn').click(function () {
-		if (($("#audio").is(':visible')) || ($("#video").is(':visible')))	{
-			$("#tag-controls-yt").hide();
-			$("#tag-controls-ap").show();
-			updateTimestamp();
-		}
-		else {
-			$("#tag-controls-ap").hide();
-			$("#tag-controls-yt").show();
-			updateTimestampYT();
-		}
-	});
-
-	// If the dropdown list is changed, change the active tab to the selected dropdown item
-	$("#file-type, #input-text").click(function() {
-		var selected = "#tabs-" + $("#file-type").val();
-    $('#text-tabs a[href="' + selected + '"]').trigger('click');
-  });
-
-	// Load YouTube Frame API
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	// Scroll to top function
-	$('#working-area').scroll(function() {
-    $('#media-playback').css('top', $(this).scrollTop());
-	});
-}(jQuery));
